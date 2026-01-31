017bd61dbe0cb95a86ebc7c127f198c88f6d3e3c
vijeth44-pro <captainmncaptain@gmail.com>
2026-01-31 20:10:53 +0530

    used ResponsiveAppBar directly inside BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import ResponsiveAppBar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <ResponsiveAppBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
